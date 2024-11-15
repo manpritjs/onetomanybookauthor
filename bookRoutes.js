5239f0219724e94aa86e845dddea6027e7fa7b2a
manpritjs <159404272+manpritjs@users.noreply.github.com>
2024-11-15 11:42:11 +0530

    Add files via upload

---
+++ bookRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const {getBook, postBook} = require("../controller/bookController")
+
+
+const router = express.Router();
+
+router.get('/:id', getBook); 
+router.post('/', postBook);
+  
+
+module.exports = router;
